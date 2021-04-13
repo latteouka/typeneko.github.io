24e1828fea7474a8265bb874559e1aa7ed05bc0f
oukalatte <oukalatte@gmail.com>
2021-04-13 11:38:12 +0800

    js

--- assets/main.js
+++ assets/main.js
@@ -18,11 +18,22 @@ function clickcopy() {
 }
 
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
-  return array
+
+  return array;
 }
 
 
